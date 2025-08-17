a06d1578c874e248575d2260017c2aeba257ba7a
Alle4n <allennajjuma96@gmail.com>
2025-08-17 19:10:12 +0300

    first commit

---
+++ interfaces/index.ts
@@ -0,0 +1,35 @@
+export interface CardProps {
+  title: string;
+  imageUrl: string;
+  description: string;
+}
+
+export interface ButtonProps {
+  label: string;
+  onClick: () => void;
+  className?: string;
+}
+
+export interface PropertyProps {
+  name: string;
+  address: {
+    state: string;
+    city: string;
+    country: string;
+  };
+  rating: number;
+  category: string[];
+  price: number;
+  offers: {
+    bed: string;
+    shower: string;
+    occupants: string;
+  };
+  image: string;
+  discount: string;
+}
+
+export interface PillProps {
+  label: string;
+  onClick?: () => void;
+}
